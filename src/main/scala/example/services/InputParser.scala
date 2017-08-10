f48c5bf111471f1f2359f4d15326008febfe2e25
Paolo Rascuna <prascuna@gmail.com>
2017-08-10 07:13:10 +0100

    InputParserTest and implementation

--- src/main/scala/example/services/InputParser.scala
+++ src/main/scala/example/services/InputParser.scala
@@ -3,9 +3,21 @@ package example.services
 import java.io.File
 
 import example.errors.AddressBookError
+import example.services.InputParser.Error.{FileNotFound, WrongArguments}
+
 
 object InputParser {
-  def parse(args: Array[String]): Either[Error, File] = ???
+  def parse(args: Array[String]): Either[Error, File] =
+    args.length match {
+      case 0 => Left(WrongArguments)
+      case s if s > 0 =>
+        val file = new File(args(0))
+        if (file.canRead && file.isFile) {
+          Right(file)
+        } else {
+          Left(FileNotFound(args(0)))
+        }
+    }
 
   sealed abstract class Error(message: String) extends AddressBookError
 
---
+++ src/test/scala/example/services/InputParserTest.scala
@@ -0,0 +1,39 @@
+package example.services
+
+import java.io.File
+
+import example.services.InputParser.Error.{FileNotFound, WrongArguments}
+import org.scalatest.{Matchers, path}
+
+class InputParserTest extends path.FunSpec with Matchers {
+
+  describe("InputParser") {
+    describe("when parsing the arguments") {
+      describe("empty array") {
+        it("should return Left(Wrong Arguments)") {
+          InputParser.parse(Array.empty[String]) shouldBe Left(WrongArguments)
+        }
+      }
+      describe("not existing file") {
+        it("should return Left(FileNotFound)") {
+          InputParser.parse(Array("does_not_exist.txt")) shouldBe Left(FileNotFound("does_not_exist.txt"))
+        }
+      }
+      describe("existing file") {
+        it("should return Right(File) containing the content of the file") {
+          val existingFileURI = getClass.getClassLoader.getResource("sample.csv").toURI
+          val expectedFile = new File(existingFileURI)
+
+          val existingFilePath = existingFileURI.getPath
+
+          val returnedFile = InputParser.parse(Array(existingFilePath))
+          returnedFile shouldBe 'right
+          returnedFile.right.foreach { file =>
+            io.Source.fromFile(file).mkString shouldBe io.Source.fromFile(expectedFile).mkString
+          }
+        }
+      }
+    }
+  }
+
+}
